16f41c7d0f18504b05478abc688bf484779b1008
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-10 11:23:34 +0330

    Update formlib.js

--- libs/js/formlib.js
+++ libs/js/formlib.js
@@ -1,10 +1,33 @@
+import bd from "https://cdn.jsdelivr.net/gh/parsa-mostafaie/betterdom@master/betterdom.js";
+
 class FormSubmitController {
   $;
   waitTabs = [];
+  ajax = null;
   constructor($el) {
+    bd.ldScript(
+      "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+    );
     this.$ = $el;
-    $el.addEventListener("submit", () => {
-      this.waitTabs.forEach((e) => e.classList.remove("d-none"));
+    let fsc = this;
+    $el.addEventListener("submit", (event) => {
+      fsc.waitTabs.forEach((e) => e.classList.remove("d-none"));
+      if (fsc.ajax) {
+        event.preventDefault();
+        var formData = new FormData(fsc.$);
+
+        $.ajax({
+          url: this.$.getAttribute("action"),
+          type: "POST",
+          data: formData,
+          success: function (data) {
+            fsc.waitTabs.forEach((e) => e.classList.add("d-none"));
+          },
+          cache: false,
+          contentType: false,
+          processData: false,
+        });
+      }
     });
   }
   SubmitWaitTab($query) {
@@ -13,10 +36,15 @@ class FormSubmitController {
     this.waitTabs.push(document.querySelector($query));
     return this;
   }
+  _ajax($ajax) {
+    if (!$ajax) return;
+    this.ajax = true;
+  }
 }
 
 document.querySelectorAll("form[submit-control]").forEach((el) => {
   let obj = new FormSubmitController(el);
   let attr = (a) => el.getAttribute("form-" + a) || undefined;
-  obj.SubmitWaitTab(attr("wait"));
+  let _ajax = el.hasAttribute("form-ajax");
+  obj.SubmitWaitTab(attr("wait"))._ajax(_ajax);
 });
