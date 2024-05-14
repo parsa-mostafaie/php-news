262ea366192ff65f36ed2ad1fb7f609aa55bb88a
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-14 19:04:42 +0330

    posts page

--- components/header.php
+++ components/header.php
@@ -11,7 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
 
-  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="<?= c_url('/assets/css/style.css') ?>" />
 </head>
 
 <body>
---
+++ posts/index.php
@@ -0,0 +1,129 @@
+<?php
+require_once '../includes/c-init.php';
+
+$exps_arr = explode('/', $_SERVER['REQUEST_URI']);
+$posts_index = array_search('posts', $exps_arr);
+$lp = array_splice($exps_arr, $posts_index + 1);
+$flp = $lp[0];
+
+if (!is_numeric($flp)) {
+  _404_();
+}
+$post_id = intval($flp);
+$post =
+  db()->TABLE('posts as p', true)
+    ->SELECT('p.title, p.content, p.image, p.verify, CONCAT(u.firstname, " ",u.lastname) as author, c.name as category')
+    ->ON('p.author = u.id', 'users as u')->ON('c.id = p.category', 'categories as c')
+    ->WHERE('p.id=?')->getFirstRow([$post_id]);
+
+if (!$post->found) {
+  _404_();
+}
+if (!$post->getColumn('verify')) {
+  _404_();
+}
+
+function comment($cid)
+{
+  $comment = db()->TABLE('comments as c', true)->
+    SELECT('CONCAT(u.firstname, " ", u.lastname) as fname, c.text')
+    ->WHERE('c.ID=' . $cid)
+    ->WHERE('verify = 1')
+    ->ON('u.ID = c.user_id', 'users as u')->getFirstRow();
+  if (!$comment->found) {
+    return '';
+  }
+  return '<div class="card bg-light-subtle mb-3">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <img src="../assets/images/profile.png" width="45" height="45" alt="user-profle">
+
+                <h5 class="card-title me-2 mb-0">' . $comment->getColumn('fname') . '</h5>
+              </div>
+
+              <p class="card-text pt-3 pr-3">
+                ' . $comment->getColumn('text') . '
+              </p>
+            </div>
+          </div>';
+}
+
+function comments($parent = 'NULL')
+{
+  global $post_id;
+  $verb = strtoupper($parent) == 'NULL' ? 'is' : '=';
+  $comments = db()->TABLE('comments as c', true)->SELECT('id')
+    ->WHERE('c.post=' . $post_id)->WHERE('c.parent ' . $verb . ' ' . $parent)
+    ->Run();
+  return $comments->fetchAll(PDO::FETCH_BOUND);
+}
+
+?>
+<?php include ('../components/header.php') ?>
+
+<!-- Content -->
+<section class="mt-4">
+  <div class="row">
+    <!-- Posts & Comments Content -->
+    <div class="col-lg-8">
+      <div class="row justify-content-center">
+        <!-- Post Section -->
+        <div class="col">
+          <div class="card">
+            <img src="../assets/images/6.jpg" class="card-img-top" alt="post-image" />
+            <div class="card-body">
+              <div class="d-flex justify-content-between">
+                <h5 class="card-title fw-bold"><?= $post->getColumn('title') ?></h5>
+                <div>
+                  <span class="badge text-bg-secondary"><?=$post->getColumn('category')?></span>
+                </div>
+              </div>
+              <p class="card-text text-secondary text-justify pt-3"><?= $post->getColumn('content') ?>
+              </p>
+              <div>
+                <p class="fs-6 mt-5 mb-0">نویسنده: <?= $post->getColumn('author') ?> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr class="mt-4" />
+
+        <!-- Comment Section -->
+        <div class="col">
+          <!-- Comment Form -->
+          <div class="card">
+            <div class="card-body">
+              <p class="fw-bold fs-5">ارسال کامنت</p>
+
+              <form>
+                <div class="mb-3">
+                  <label class="form-label">نام</label>
+                  <input type="text" class="form-control" />
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">متن کامنت</label>
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-dark">
+                  ارسال
+                </button>
+              </form>
+            </div>
+          </div>
+
+          <hr class="mt-4" />
+          <!-- Comment Content -->
+          <p class="fw-bold fs-6">تعداد کامنت : <?= count(comments()) ?> </p>
+
+           <?php foreach (comments() as $comm) {
+             echo comment($comm);
+           } ?>
+        </div>
+      </div>
+    </div>
+
+     <?php include ('../components/sidebar.php') ?>
+  </div>
+</section>
+<?php include ('../components/footer.php') ?>
