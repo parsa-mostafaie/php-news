137cd41fdee78246e2a7ad9d6dc397108e966a65
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-16 14:38:49 +0330

    Recently Posts index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php include ('components/header.php') ?>
+<?php
+$__component__post_pdos = db()->TABLE('posts', alias: 'p')->SELECT('*')
+  ->LIMIT(15)->Run();
+?>
 <!-- Slider Section -->
 <section>
   <div id="carousel" class="carousel slide">
@@ -56,81 +60,9 @@
     <!-- Posts Content -->
     <div class="col-lg-8">
       <div class="row g-3">
-        <div class="col-sm-6">
-          <div class="card">
-            <img src="./assets/images/4.jpg" class="card-img-top" alt="post-image" />
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <h5 class="card-title fw-bold">لورم ایپسوم</h5>
-                <div>
-                  <span class="badge text-bg-secondary">طبیعت</span>
-                </div>
-              </div>
-              <p class="card-text text-secondary pt-3">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
-                چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون
-                بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
-                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با
-                هدف بهبود
-              </p>
-              <div class="d-flex justify-content-between align-items-center">
-                <a href="single.php" class="btn btn-sm btn-dark">مشاهده</a>
-
-                <p class="fs-7 mb-0">نویسنده : علی شیخ</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <img src="./assets/images/5.jpg" class="card-img-top" alt="post-image" />
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <h5 class="card-title fw-bold">لورم ایپسوم</h5>
-                <div>
-                  <span class="badge text-bg-secondary">گردشگری</span>
-                </div>
-              </div>
-              <p class="card-text text-secondary pt-3">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
-                چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون
-                بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
-                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با
-                هدف بهبود
-              </p>
-              <div class="d-flex justify-content-between align-items-center">
-                <a href="single.php" class="btn btn-sm btn-dark">مشاهده</a>
-
-                <p class="fs-7 mb-0">نویسنده : علی شیخ</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-            <img src="./assets/images/6.jpg" class="card-img-top" alt="post-image" />
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <h5 class="card-title fw-bold">لورم ایپسوم</h5>
-                <div>
-                  <span class="badge text-bg-secondary">متفزقه</span>
-                </div>
-              </div>
-              <p class="card-text text-secondary pt-3">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
-                چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون
-                بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
-                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با
-                هدف بهبود
-              </p>
-              <div class="d-flex justify-content-between align-items-center">
-                <a href="single.php" class="btn btn-sm btn-dark">مشاهده</a>
-
-                <p class="fs-7 mb-0">نویسنده : علی شیخ</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <?php
+        include ($_SERVER['DOCUMENT_ROOT'] . c_url('/components/posts.php'))
+          ?>
       </div>
     </div>
 
