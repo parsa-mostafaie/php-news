d708abd35ba9ab61197ff13d3b5c68e54075bb3e
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-21 13:35:32 +0330

    `Edit Post`

--- admin/pages/posts/edit.php
+++ admin/pages/posts/edit.php
@@ -1,53 +1,85 @@
 <?php $current_page = '/posts' ?>
+<?php require_once '../../../includes/c-init.php';
+
+$post_qs = get_val('post');
+
+if (!is_numeric($post_qs)) {
+  _404_();
+}
+$post_id = intval($post_qs);
+$post =
+  db()->TABLE('posts', true, 'p')
+    ->SELECT('p.date, p.ID, p.title, p.content, p.image, p.verify, CONCAT(u.firstname, " ",u.lastname) as author, c.name as category, c.id as cid')
+    ->ON('p.author = u.id', 'users as u')->ON('c.id = p.category', 'categories as c')
+    ->WHERE('p.id=?')->getFirstRow([$post_id]);
+
+if (!$post->found) {
+  _404_();
+}
+
+['n' => $sessn, 'v' => $sessv] = secure_form();
+?>
 <?php include '../../components/header.php' ?>
 <!-- Main Section -->
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mb-5">
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="fs-3 fw-bold">ویرایش مقاله</h1>
   </div>
 
   <!-- Posts -->
   <div class="mt-4">
-    <form class="row g-4">
+    <form class="row g-4" submit-control form-wait="#wait" action="edit-backend.php" enctype="multipart/form-data">
+      <input type="hidden" name="sec_form_sess_n" value="<?= $sessn ?>">
+      <input type="hidden" name="sec_form_sess_v" value="<?= $sessv ?>">
+      <input type="hidden" name="post" value="<?= $post_id ?>" ?>
       <div class="col-12 col-sm-6 col-md-4">
-        <label class="form-label">عنوان مقاله</label>
-        <input type="text" class="form-control" value="لورم ایپسوم" />
+        <label class="form-label" for="title">عنوان مقاله</label>
+        <input type="text" class="form-control" id="title" name="title" value="<?= $post->getColumn('title') ?>" />
       </div>
 
       <div class="col-12 col-sm-6 col-md-4">
-        <label class="form-label">نویسنده مقاله</label>
-        <input type="text" class="form-control" value="علی شیخ" />
+        <label class="form-label" for="author">نویسنده مقاله</label>
+        <input type="text" class="form-control" disabled id="author" value="<?= $post->getColumn('author') ?>" />
       </div>
 
       <div class="col-12 col-sm-6 col-md-4">
         <label class="form-label">دسته بندی مقاله</label>
-        <select class="form-select">
-          <option value="1">طبیعت</option>
-          <option value="2">گردشگری</option>
-          <option value="3">تکنولوژی</option>
-          <option value="4">متفرقه</option>
-        </select>
+        <?php categories_sel(default: $post->getColumn('cid')) ?>
       </div>
 
       <div class="col-12 col-sm-6 col-md-4">
-        <label for="formFile" class="form-label">تصویر مقاله</label>
-        <input class="form-control" type="file" />
+        <label for="formFile" class="form-label">تصویر جدید مقاله</label>
+        <input class="form-control" type="file" name="photo" />
       </div>
 
       <div class="col-12">
-        <label for="formFile" class="form-label">متن مقاله</label>
-        <textarea class="form-control" rows="8">
-لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-                            </textarea>
+        <label for="content" class="form-label">متن مقاله</label>
+        <textarea class="form-control" rows="6" name="content"
+          id="content"><?= $post->getColumn('content') ?></textarea>
       </div>
 
-      <div class="col-12 col-sm-6 col-md-4">
-        <img class="rounded" src="../../assets/images/1.jpg" width="300" />
+
+      <div class="col-12 col-sm-6 col-md-4 d-flex flex-column">
+        <?= $post->getAssetBasedCol('image')->get_img('class = "rounded w-100"', web_url(c_url('/assets/images/1.jpg'))) ?>
+        <b class="text-dark text-center mt-1">تصویر فعلی</b>
       </div>
 
+      <div id="wait">لطفا صبر کنید!</div>
+      <div id="error" class='text-danger'></div>
       <div class="col-12">
-        <button type="submit" class="btn btn-dark">ویرایش</button>
+        <button type="submit" class="btn btn-dark" ajax-submit name='edit'>ویرایش</button>
       </div>
+      <script src="/libs/pluslib/frontend/formlib.js" type="module"></script>
+      <script>
+        window.addEventListener("load", () => {
+          let e = document.querySelector("#error");
+          window.FormLibInitializer.setting(
+            "[ajax-submit]",
+            () => (e.textContent = ""),
+            ({ err }) => (e.textContent = JSON.stringify(err))
+          ).init();
+        });
+      </script>
     </form>
   </div>
 </main>
