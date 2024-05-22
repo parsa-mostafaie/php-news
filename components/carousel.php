1e8b78b6ad52245d0ddf6f203c4c7478dcbf8905
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-22 09:41:00 +0330

    carousel

---
+++ components/carousel.php
@@ -0,0 +1,33 @@
+<?php include_once __DIR__ . ('/../includes/c-init.php')
+  // __component__caro_pdos
+;
+?>
+<div id="carousel" class="carousel slide">
+  <div class="carousel-indicators">
+    <?php foreach ($__component__caro_pdos as $i => $post): ?>
+      <button type="button" data-bs-target="#carousel" data-bs-slide-to="<?= $i ?>"
+        class="<?= $i == 0 ? 'active' : '' ?>"></button>
+    <?php endforeach; ?>
+  </div>
+  <div class="carousel-inner rounded">
+    <?php foreach ($__component__caro_pdos as $i => $post): ?>
+      <div class="carousel-item overlay carousel-height <?= $i == 0 ? 'active' : '' ?>">
+        <?= imageComponent($post['Image'], 'class="d-block w-100" alt="post-image"', web_url(c_url('/assets/images/1.jpg'))) ?>
+        <div class="carousel-caption d-none d-md-block">
+          <h5><?= $post['Title'] ?></h5>
+          <p>
+            <?= $post['description'] ?>
+          </p>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="next">
+    <span class="carousel-control-prev-icon"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+    <span class="carousel-control-next-icon"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
--- index.php
+++ index.php
@@ -2,56 +2,13 @@
 <?php
 $__component__post_pdos = db()->TABLE('posts', alias: 'p')->SELECT('*')
   ->LIMIT(15)->ORDER_BY('date DESC')->Run();
+
+$__component__caro_pdos = db()->TABLE('posts', alias: 'p')->SELECT('*')
+  ->LIMIT(3)->ORDER_BY('date DESC')->Run()->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!-- Slider Section -->
 <section>
-  <div id="carousel" class="carousel slide">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="1"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="2"></button>
-    </div>
-    <div class="carousel-inner rounded">
-      <div class="carousel-item overlay carousel-height active">
-        <img src="./assets/images/1.jpg" class="d-block w-100" alt="post-image" />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>لورم ایپسوم متن</h5>
-          <p>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
-            با استفاده
-          </p>
-        </div>
-      </div>
-      <div class="carousel-item carousel-height overlay">
-        <img src="./assets/images/2.jpg" class="d-block w-100" alt="post-image" />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>لورم ایپسوم متن</h5>
-          <p>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
-            با استفاده
-          </p>
-        </div>
-      </div>
-      <div class="carousel-item carousel-height overlay">
-        <img src="./assets/images/3.jpg" class="d-block w-100" alt="post-image" />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>لورم ایپسوم متن</h5>
-          <p>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
-            با استفاده
-          </p>
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+  <?php include ('components/carousel.php'); ?>
 </section>
 
 <!-- Content Section -->
