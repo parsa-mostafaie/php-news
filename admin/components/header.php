067c1fa22e47b00a45637c5cd8b8e54189e8f84c
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-23 13:13:11 +0330

    Use all plugins for tinymce

--- admin/components/header.php
+++ admin/components/header.php
@@ -22,11 +22,11 @@
     <script>
       tinymce.init({
         selector: 'textarea#tiny',
-        // plugins: [
-        //   'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'markdown',
-        //   'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
-        //   'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
-        // ],
+        plugins: [
+          'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'markdown',
+          'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+          'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        ],
         toolbar: 'undo redo | a11ycheck casechange blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify |' +
           'bullist numlist checklist outdent indent | removeformat | code table help'
       })
