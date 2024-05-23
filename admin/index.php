24bf8c727ece00841b9c1188e509ca88a09ca949
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-23 18:59:38 +0330

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
 <!-- Main Section -->
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="fs-3 fw-bold">داشبورد</h1>
+    <h1 class="fs-3 fw-bold">پنل ادمین</h1>
   </div>
 
   <!-- Recently Posts -->
