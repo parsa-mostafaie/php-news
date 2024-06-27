76b7aa491c6cd2282cedb94fcb505dc9ebccf4c4
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-27 16:25:37 +0330

    Update view.php

--- libs/MVC/view.php
+++ libs/MVC/view.php
@@ -1,14 +1,14 @@
 <?php
 defined('ABSPATH') || exit;
 
-function view($url)
+function view($url, $id = null)
 {
   $url = c_url('/Views/' . $url);
   $etc = etc_url($url);
   $www = www_url($url);
 
   ?>
-  <div ajax-container>
+  <div ajax-container id="<?= $id ?? basename($url) ?>">
     <div class="loading">لطفا صبر کنید!</div>
     <div ajax-content href="<?= $www ?>" loading=".loading">
       <?= file_get_contents($www) ?><!-- For SEO -->
