b4afc756f00ddcc6c344afa166ac3213e7096ae9
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-28 11:21:30 +0330

    dangerbtns()

--- assets/js/dangerbtn.js
+++ assets/js/dangerbtn.js
@@ -1,21 +1,26 @@
-document.querySelectorAll("[danger-btn]").forEach((db) => {
-  db.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.pluslib_wait = true;
-    Swal.fire({
-      title: "مطمئن هستی؟",
-      text: "این قابل بازگشت نیست!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "آره انجامش بده",
-      cancelButtonText: "نه!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        event.pluslib_actions?.forEach((action) => action());
-        event.pluslib_actions || (location.href = db.getAttribute("href"));
-      }
+function dangerbtns() {
+  document.querySelectorAll("[danger-btn]:not([db-eh])").forEach((db) => {
+    db.addEventListener("click", (event) => {
+      event.preventDefault();
+      event.pluslib_wait = true;
+      Swal.fire({
+        title: "مطمئن هستی؟",
+        text: "این قابل بازگشت نیست!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "آره انجامش بده",
+        cancelButtonText: "نه!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          event.pluslib_actions?.forEach((action) => action());
+          event.pluslib_actions || (location.href = db.getAttribute("href"));
+        }
+      });
     });
+    db.setAttribute("db-eh", "set");
   });
-});
+}
+
+dangerbtns();
