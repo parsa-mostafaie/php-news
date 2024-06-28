1dff56a524272a42253d492dfd54fe60383ce95b
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-28 11:21:58 +0330

    Remove not needed comments

--- components/shared/dashboard_admin/sidebar.php
+++ components/shared/dashboard_admin/sidebar.php
@@ -23,60 +23,6 @@ include_once 'templates.php';
     </div>
 
     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-      <!-- <ul class="nav flex-column pe-3">
-        <li class="nav-item has-submenu">
-          <a class="<?= sidebarItemClass ?> <?= classIt('/') ?>" href="<?= $dashboard ?>">
-            <i class="bi bi-house-fill fs-4 text-secondary"></i>
-            <span class="fw-bold">داشبورد</span>
-          </a>
-          <ul class="submenu <?= supermenu_classit('dashboard') ?>">
-            <li class="nav-item">
-              <a class="nav-link link-body-emphasis text-decoration-none d-flex align-items-center gap-2 <?= classIt('/posts') ?>"
-                href="<?= $dashboardPages ?>posts/index.php">
-                <i class="bi bi-file-earmark-image-fill fs-4 text-secondary"></i>
-                <span class="fw-bold">مقالات</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link link-body-emphasis text-decoration-none d-flex align-items-center gap-2  <?= classIt('/com') ?>"
-                href="<?= $dashboardPages ?>comments/index.php">
-                <i class="bi bi-chat-left-text-fill fs-4 text-secondary"></i>
-
-                <span class="fw-bold">کامنت ها</span>
-              </a>
-            </li>
-          </ul>
-        </li>
-        <?php if (isAdmin()): ?>
-          <li class="nav-item">
-            <a class="nav-link link-body-emphasis text-decoration-none d-flex align-items-center gap-2"
-              href="<?= c_url('/admin/') ?>">
-              <i class="bi bi-person-check-fill fs-4 text-secondary"></i>
-              <span class="fw-bold">پنل ادمین</span>
-            </a>
-          </li>
-        <?php endif ?>
-        <li class="nav-item">
-          <a class="nav-link link-body-emphasis text-decoration-none d-flex align-items-center gap-2"
-            href="<?= c_url('/') ?>">
-            <i class="bi bi-newspaper fs-4 text-secondary"></i>
-
-            <span class="fw-bold">اخبار</span>
-          </a>
-        </li>
-
-
-        <li class="nav-item">
-          <a class="nav-link link-body-emphasis text-decoration-none d-flex align-items-center gap-2"
-            href="<?= c_url('/auth/signout.php') ?>">
-            <i class="bi bi-box-arrow-right fs-4 text-secondary"></i>
-
-            <span class="fw-bold">خروج</span>
-          </a>
-        </li>
-      </ul>
-        -->
       <?php navGenerate($sidebarTemplate); ?>
     </div>
   </div>
