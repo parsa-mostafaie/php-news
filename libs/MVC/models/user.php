6657c1695d52bee5adb91dcf2c9cb0b9218fce2e
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-03 16:40:29 +0330

    Update user.php

--- libs/MVC/models/user.php
+++ libs/MVC/models/user.php
@@ -9,4 +9,31 @@ class User extends UserBase
     'posts' => array(self::HAS_MANY, 'Post', 'author'),
     'comments' => array(self::HAS_MANY, 'Comment', 'user_id'),
   );
+
+  function role()
+  {
+    return @UserRole::from($this->admin);
+  }
+
+  function changeRole(UserRole|int $role, User|null $upgrader = null)
+  {
+    $asInt = is_int($role) ? $role : $role->value;
+    if (is_null($upgrader) || ($asInt <= $upgrader->admin && $this->admin <= $upgrader->admin)) {
+      $this->admin = $asInt;
+    }
+  }
+
+  function auth(UserRole|int $minRole)
+  {
+    $asInt = is_int($minRole) ? $minRole : $minRole->value;
+
+    return $this->admin >= $asInt;
+  }
+}
+
+enum UserRole: int
+{
+  case Admin = 2;
+  case Normal = 0;
+  case Writer = 1;
 }
