a52d8f35d2f586e9393dc24799ad65f721d5e5a6
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-31 13:49:31 +0330

    Use bootstrap-spinner in ajax loading

--- libs/view.php
+++ libs/view.php
@@ -10,7 +10,10 @@ function view($url, $id = null, $props = [])
 
   ?>
   <div ajax-container id="<?= $id ?? pathinfo($etc, PATHINFO_FILENAME) ?>">
-    <div class="loading">لطفا صبر کنید!</div>
+    <div class="d-flex align-items-center overflow-hidden justify-content-center mx-auto loading">
+      <div class="spinner-grow text-primary" role="status" aria-hidden="true"></div>
+      <p class="my-0 me-2">لطفا صبر کنید!</p>
+    </div>
     <div ajax-content http-method="post" href="<?= redirect($www, back: true, gen: true) ?>" loading=".loading">
     </div>
   </div>
