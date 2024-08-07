31f359440f168df2e207b22a33474c7722fae94a
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-07 14:03:38 +0330

    [ADD] Helper: `tablify_pro`

--- libs/@table.php
+++ libs/@table.php
@@ -66,3 +66,64 @@ function tablify(
     </tbody>
   </table><?php
 }
+
+
+define('TABLE_BASE_CSS_CLASS', 'table table-hover align-middle text-nowrap');
+
+define('TABLE_CONTAINER_BASE_CSS_CLASS', 'table-responsive small');
+
+/**
+ * Convert array of models into HTML Table
+ * 
+ * @param array $fields
+ * @param array $values
+ * @param callable $render
+ * @param string $table_class
+ * @param string $container_class
+ * @return bool|void
+ */
+function tablify_pro(
+  $fields = [],
+  $values = [],
+  $render = null,
+  $empty_msg = null,
+  $table_class = TABLE_BASE_CSS_CLASS,
+  $container_class = TABLE_CONTAINER_BASE_CSS_CLASS
+) {
+  $render ??= fn($model, $tdr) => $tdr($model->toArray());
+
+  if (count($values) == 0) {
+    echo value($empty_msg);
+    return false;
+  }
+
+  $td_render = function ($values) {
+    $values = wrap($values);
+    ?>
+    <?php foreach ($values as $value): ?>
+      <td><?= value($value) ?></td>
+    <?php endforeach; ?>
+  <?php
+  }
+
+    ?>
+  <div class="<?= $container_class ?>">
+    <table class="<?= $table_class ?>">
+      <thead>
+        <tr>
+          <?php foreach ($fields as $field): ?>
+            <th><?= value($field) ?></th>
+          <?php endforeach; ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($values as $value): ?>
+          <tr>
+            <?= $render($value, $td_render) ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+  <?php
+}
