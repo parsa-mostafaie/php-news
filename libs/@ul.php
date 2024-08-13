848b9371b2c9fcbc42b9af4428665603345049d6
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-13 20:07:34 +0330

    Rewrite `listify` using Model System

--- libs/@ul.php
+++ libs/@ul.php
@@ -1,32 +1,40 @@
 <?php
 
+use App\Models\Category;
+
 defined('ABSPATH') || exit;
 
-function listify(PDOStatement $st, $col, $link = '#', $class = '', $echo = true)
+function listify($models, $col, $link = '#', $class = '')
 {
-  $html = '<ul class="list-group list-group-flush p-0">';
-  $fetch = $st->fetchAll(PDO::FETCH_ASSOC);
-  foreach ($fetch as $row) {
-    $colv = $row[$col];
-    $link_ = valueof($link, $row);
-    $class_ = valueof($class, $row);
-    $html .= "<li class='list-group-item'><a class='link-body-emphasis text-decoration-none $class_' href='$link_'>$colv</a></li>";
-  }
-  $html .= '</ul>';
-  if ($echo)
-    echo $html;
-  return $html;
+  ?>
+  <ul class="list-group list-group-flush p-0">
+    <?php
+    foreach ($models as $model) {
+      $colv = $model->$col;
+      $link_ = valueof($link, $model);
+      $class_ = valueof($class, $model);
+      ?>
+      <li class='list-group-item'>
+        <a class='link-body-emphasis text-decoration-none <?= $class_ ?>' href='<?= $link_ ?>'><?= $colv ?></a>
+      </li>
+      <?php
+    } ?>
+  </ul>
+  <?php
 }
 
 
 function categories_list()
 {
-  $st = db()->TABLE('categories')->SELECT(['ID', 'Name'])->Run();
-  $href = function ($row) {
-    return c_url("/search.php?search&cat=" . $row['ID']);
+  $st = Category::all();
+
+  $href = function (Category $category) {
+    return $category->get_url();
   };
-  $class = function ($row) {
-    return get_val('cat') == $row['ID'] ? 'fw-bold' : '';
+
+  $class = function (Category $category) {
+    return get_val('cat') == $category->_id() ? 'fw-bold' : '';
   };
+
   listify($st, 'Name', $href, $class);
 }
