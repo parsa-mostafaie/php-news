c5ea6fa1c05a3691d5b0aad8d323ab51c904b5cc
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-16 21:03:12 +0330

    View: retry

--- libs/view.php
+++ libs/view.php
@@ -8,16 +8,20 @@ function view($url, $id = null, $props = [])
   $etc = etc_url($url);
   $www = www_url($url);
 
-  ?>
-  <div ajax-container id="<?= $id ?? pathinfo($etc, PATHINFO_FILENAME) ?>">
+  $id = pathinfo($etc, PATHINFO_FILENAME)
+
+    ?>
+  <div ajax-container id="<?= $id ?>">
     <div class="d-flex align-items-center overflow-hidden justify-content-center mx-auto loading">
       <div class="spinner-grow text-primary" role="status" aria-hidden="true"></div>
       <p class="my-0 me-2">لطفا صبر کنید!</p>
     </div>
     <div class="fallback text-danger d-none d-flex justify-content-center align-items-center mx-auto">
       خطا هنگام بارگزاری...
+      &nbsp;<a class="text-decoration-underline fw-bold" href ajax-reload="#<?= $id ?>" onclick="return false;">تلاش مجدد</a>
     </div>
-    <div ajax-content http-method="post" href="<?= redirect($www, back: true, gen: true) ?>" loading=".loading" fallback=".fallback">
+    <div ajax-content http-method="post" href="<?= redirect($www, back: true, gen: true) ?>" loading=".loading"
+      fallback=".fallback">
     </div>
   </div>
   <?php
