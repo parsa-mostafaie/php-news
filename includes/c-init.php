9f65417def7cb165a55d62e6555501765de4f56a
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-09-14 16:08:08 +0330

    Use Pluslib via Composer

--- includes/c-init.php
+++ includes/c-init.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/libs/pluslib/init.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/libs/jdf/jdf.php';
 require_once "boot.php";
 
